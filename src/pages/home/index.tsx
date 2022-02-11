801079b23fd74daef80e3dde0ae56ee4fd9edc7f
ArjDev69 <bruno96araujo@gmail.com>
2022-02-11 18:53:42 -0300

    feat: add first view

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const home: React.FC = () => {
+  return (
+    <div>
+      Teste Aqui 23
+    </div>
+  )
+}
+
+export default home;
