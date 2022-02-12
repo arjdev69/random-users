8c1c739adaf5b408826f7e38a9a2dd798b81c68b
ArjDev69 <bruno96araujo@gmail.com>
2022-02-12 00:15:06 -0300

    feat: started requests data

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useDispatch, useSelector } from "react-redux";
+
+import { getListUsersSuccess } from '@/store/modules/users/actions'
 
 import Styles from './home-styles.scss';
 
 const home: React.FC = () => {
+  const dispatch = useDispatch();
+  const users = useSelector((state: any) => state.users);
+
+  const fetchApi = () => {
+    dispatch(getListUsersSuccess());
+  };
+
+  useEffect(() => {
+   fetchApi();
+  }, [])
+  
   return (
     <div className={Styles.home}>
       Home
