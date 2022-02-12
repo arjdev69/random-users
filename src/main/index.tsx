d4a4fe3f2d826f511dffcbd00de2e99e379d0a13
ArjDev69 <bruno96araujo@gmail.com>
2022-02-11 23:30:55 -0300

    feat: setup redux provider

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+
+import '@/config/ReactotronConfig';
+
+import { store, persistor } from '@/store';
+
 import Home from '@/pages/home'
 
 ReactDOM.render(
-  <Home />,
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <Home />,
+      </PersistGate>
+  </Provider>,
   document.getElementById('main')
 )
