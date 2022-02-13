791109aa11516a0ed58ffc06ee1bafb3a78def4b
ArjDev69 <bruno96araujo@gmail.com>
2022-02-13 18:36:17 -0300

    fix: changed path home page in routers

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-import Home from "@/pages/home";
+import Home from "@/pages/Home/Home";
 import Notfound from "@/pages/notfound";
 import { Header } from "@/components";
 
