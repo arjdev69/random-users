46638d9f297b87956078dd8d31a8af8f13a7ce74
ArjDev69 <bruno96araujo@gmail.com>
2022-02-16 02:19:18 -0300

    feat: started search component

--- src/container/BoxUsers/BoxUsers.tsx
+++ src/container/BoxUsers/BoxUsers.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import _ from 'lodash';
 
 import { LABELS } from '@/utils/Contants'
 
+import { MdSearch, MdBackspace, MdAddCircle } from 'react-icons/md';
 import { GridContent, Loading } from '@/components';
 
-import Styles from '../Styles/container-styles.scss'
 
-import {logo}  from '@/assets'
+import Styles from '../Styles/container-styles.scss'
+import Input from '../Styles/input-styles.scss'
+
+import { logo } from '@/assets'
 
 interface Props {
   usersList: any[],
@@ -17,38 +20,62 @@ interface Props {
 }
 
 const BoxUsers: React.FC<Props> = ({ usersList, clear, callback }) => {
+  const [dataList, setDataList] = useState([])
+
+  useEffect(() => {
+    setDataList(usersList)
+  }, [usersList])
 
   return (
     <div className={Styles.box}>
       <div className={Styles.topBox}>
-        <img
-          src={logo}
-          height="48"
-          width="48"
-        />
-        <span className={Styles.title}>
-          {LABELS.boxUser.labels.titleBox}
-        </span>
-      </div>
-      <div id="contentBox" className={Styles.contentBox}>
-        {
-          _.isEmpty(usersList)
-            ? <span>{LABELS.boxUser.labels.noItems}</span>
-            : <GridContent data={usersList} />
-        }
-      </div>
-      <div className={Styles.footerBox}>
-        <span>Total users: {usersList.length}</span>
-        <div>
+        <div className={Styles.groupColumn}>
+          <img
+            src={logo}
+            height="48"
+            width="48"
+          />
+          <span className={Styles.text}>
+            {LABELS.boxUser.labels.titleBox}
+          </span>
+        </div>
+        <div className={Styles.groupButton}>
+          <span className={Styles.text}>Users: {dataList.length}</span>
           <button
             onClick={clear}
             className={`${Styles.btnClear}`}>
-            -
+            <MdBackspace size={24} />
           </button>
+        </div>
+      </div>
+      <div id="contentBox" className={Styles.contentBox}>
+        {
+          _.isEmpty(dataList)
+            ? <span>{LABELS.boxUser.labels.noItems}</span>
+            : <GridContent data={dataList} />
+        }
+      </div>
+      <div className={Styles.footerBox}>
+        <div className={Styles.boxFinder}>
+          <div className={Input.searchBox}>
+            <button className={Input.btnSearch}>
+              <MdSearch size={24} />
+            </button>
+            <input 
+              type="text"
+              className={Input.inputSearch}
+              placeholder="Type to Search..." 
+              onChange={(ev) => {
+                console.log(ev.target.value)
+              }}
+            />
+          </div>
+        </div>
+        <div className={Styles.groupButton}>
           <button
             onClick={callback}
             className={Styles.button}>
-            +
+            <MdAddCircle size={40} />
           </button>
         </div>
       </div>
